d2c2209f8240c74566f2fc4322b995eebc44d3b5
Nitin Chakrawarti <149225785+NitinChakrawarti@users.noreply.github.com>
2024-09-16 16:23:10 +0530

    join  us section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,10 @@ module.exports = {
   theme: {
 
     screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '960px',
-      xl: '1200px'
+      "sm": '640px',
+      "md": '768px',
+      "lg": '960px',
+      "xl": '1200px'
     },
 
     extend: {
