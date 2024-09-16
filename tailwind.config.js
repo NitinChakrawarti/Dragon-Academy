acc65511c75b2ab460f496a585dd75395bfef201
Nitin Chakrawarti <149225785+NitinChakrawarti@users.noreply.github.com>
2024-09-16 09:52:38 +0530

    home page 50%  done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [".src/*.html"],
+  content: ["./*.html"],
   theme: {
-   
+
     screens: {
       sm: '640px',
       md: '768px',
       lg: '960px',
       xl: '1200px'
     },
-    
+
     extend: {
+      dropShadow: {
+        '3xl': '0 35px 35px rgba(0, 0, 0, 1)',
+        '4xl': [
+            '0 35px 35px rgba(0, 0, 0, 1)',
+            '0 45px 65px rgba(0, 0, 0, 1)'
+        ]
+      },
       padding: {
-        "default": '20PX',
+        "default": '20px',
       },
       colors: {
         'primary': "#75A47F",
-        'secondary' : "#FA7070",
+        'secondary': "#FA7070",
         'tert': "#F3FDE8",
-        'accent': "#967E76"
+        'accent': "#967E76",
+        "bg": "#231b25"
       },
       fontFamily: {
         'primary': ['Cinzel'],
         'secondary': ['Merriweather']
+
       },
-     
+
     },
   },
   plugins: [],
